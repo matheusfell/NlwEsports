4672ffb2139e0b7cfbde9a8435d00946a322a749
Matheus Fellipe <bentomatheus48@gmail.com>
2022-09-16 16:32:22 -0300

    commit

--- src/server.mjs
+++ src/server.mjs
@@ -6,7 +6,9 @@ const app = express();
 
 app.get("/ads", (request, response) => {
   return response.json([
-    {id: 1, name: 'Anúncio 1'},
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
   ]);
   console.log("Acessou Ads!");
 });
