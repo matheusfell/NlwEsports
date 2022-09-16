a3017b11b6848c9def7da3c72a29a929dcee9e23
Matheus Fellipe <bentomatheus48@gmail.com>
2022-09-16 16:26:49 -0300

    commit

--- src/server.mjs
+++ src/server.mjs
@@ -1,9 +1,14 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-// www.minhaapi.com/ads
+// localhost:3333/ads
 
-app.get('/ads', () => {
-    
-})
+app.get("/ads", (request, response) => {
+  return response.json([
+    {id: 1, name: 'Anúncio 1'},
+  ]);
+  console.log("Acessou Ads!");
+});
+
+app.listen(3333);
